996a272e62696dfd39fd8fa51a3753f7c9022072
noe.vernier <noe.vernier@telecom-paris.fr>
2023-01-17 18:12:21 +0100

    Initial Commit

---
+++ CryptoHandle/CryptoHandleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoHandleApp.swift
+//  CryptoHandle
+//
+//  Created by Noé VERNIER on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoHandleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
